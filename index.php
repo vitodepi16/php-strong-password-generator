b62a25fb1e438e37aad17890f50b62dace852778
vitodepi16 <118888625+vitodepi16@users.noreply.github.com>
2023-05-05 17:30:27 +0200

    try to fix SESSION

--- index.php
+++ index.php
@@ -8,11 +8,11 @@ include './partial/function/function.php';
 <?php include './partial/template/header.php' ?>
 
 <h1 class="text-upperrcase  ">Generatore di password</h1>
-<form class="row" action="<?php echo $_SERVER['PHP_SELF'] ?>" method="GET">
+<form class="row" action="<?php echo $_SERVER['ShowPassword.php'] ?>" method="GET">
     <div class="col">
         <label for="inputEmail4" class="form-label">Quanto vuoi lunga la tua nuova password?</label>
         <input type="number" name="number" class="form-control ms-2" placeholder="Inserisci numero" aria-label="number">
-        <button type="submit" class="btn btn-success m-2 ">Invia</button>
+        <button type="submit" class="btn btn-success m-2" href="./ShowPassword.php">Invia</button>
     </div>
 </form>
 
